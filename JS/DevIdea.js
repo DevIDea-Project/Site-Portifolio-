565bbe05d934c367d7ac0d72cefe25c20d05e2cc
BrunoVinicius <brunoviniciustica@gmail.com>
2019-01-24 14:55:28 -0300

    Primeiro Commit de projeto de Site para Portifolio

---
+++ JS/DevIdea.js
@@ -0,0 +1,27 @@
+var $lis  = document.getElementById('git'); 
+
+var $img1 = document.getElementById('node');
+var $img2 = document.getElementById('java1');
+var $img3 = document.getElementById('java2');
+var $img4 = document.getElementById('html');
+
+$lis.addEventListener('click', function(){
+    window.location.href = 'https://github.com/brunoViniciusTiCa';
+ });
+
+$img1.addEventListener('click', function(){
+    window.location.href = 'https://github.com/brunoViniciusTiCa/Projeto-Node';
+ });
+
+ $img2.addEventListener('click', function(){
+    window.location.href = 'https://github.com/brunoViniciusTiCa/JAVA-Biblioteca-Projeto-POO';
+ });
+
+ $img3.addEventListener('click', function(){
+    window.location.href = 'https://github.com/brunoViniciusTiCa/Burgueria';
+ });
+
+$img4.addEventListener('click', function(){
+    window.location.href = 'https://github.com/brunoViniciusTiCa/Curso-de-JavaScript';
+ });
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto-Site-Portf-lio
+Repositório no git para controle das versões do nosso site como portfólio.
